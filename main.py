b1fdf4a4b157b05c9d728480f42565e0e9693851
Aude <101478194+alalab12@users.noreply.github.com>
2026-02-26 15:07:50 +0100

    Integrate Floyd-Warshall algorithm into main
    Added Floyd-Warshall algorithm to compute shortest paths.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from graph import load_graph, print_matrix
+from floyd import floyd_warshall
 
 
 def main():
@@ -9,12 +10,21 @@ def main():
 
     file = os.path.join(dir,filename)
 
-    result = load_graph(file)
-    num_vertices = result[0]
-    distance_matrix = result[1]
+    load = load_graph(file)
+    num_vertices = load[0]
+    distance_matrix = load[1]
 
 
     print_matrix(distance_matrix, label="Matrice des poids initiale (L)")
+
+    #Appel de Floyd-Warshall
+    fw = floyd_warshall(distance_matrix)
+    L = fw[0]   # matrice des distances minimales
+    P = fw[1]   # matrice des successeurs
+
+
+    print_matrix(L, label="Matrice L finale (distances minimales)")
+    print_matrix(P, label="Matrice P finale (successeurs)")
     
 
 if __name__ == "__main__" :
